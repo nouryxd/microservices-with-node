8119b3006dcc027e95d6b5f8f1693d92235b9463
lyx0 <stefan@parfuss.me>
2021-07-17 13:41:20 +0200

    add /query route

--- 02-mini-microservice-app/blog/query/index.js
+++ 02-mini-microservice-app/blog/query/index.js
@@ -1,19 +1,36 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/post', (req, res) => {
+const posts = {};
 
+app.get("/post", (req, res) => {
+  res.send(posts);
 });
 
-app.post('/events', (req, res) => {
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
 
+  if (type === "PostCreated") {
+    const { id, title } = data;
+
+    posts[id] = { id, title, comments: [] };
+  }
+
+  if (type === "CommentCreated") {
+    const { id, content, postId } = data;
+
+    const post = posts[postId];
+    post.comments.push({ id, content });
+  }
+  console.log(posts);
+  res.send({});
 });
 
 app.listen(4002, () => {
-  console.log('Listening on port 4002')
-})
+  console.log("Listening on port 4002");
+});
