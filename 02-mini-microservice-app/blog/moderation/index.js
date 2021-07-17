3e6e6a02d50b7e7f4993710a455152a87da94996
lyx0 <stefan@parfuss.me>
2021-07-17 16:10:09 +0200

    being start of moderation

---
+++ 02-mini-microservice-app/blog/moderation/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post("/event", (req, res) => {
+  
+});
+
+app.listen(4003, () => {
+  console.log("Listening on port 4003");
+});
