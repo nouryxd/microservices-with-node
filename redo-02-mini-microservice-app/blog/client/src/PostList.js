a343dff16fdfbd2c6a56addbae0d816b5e750d27
lyx0 <stefan@parfuss.me>
2021-08-08 19:22:16 +0200

    add CommentCreate

--- redo-02-mini-microservice-app/blog/client/src/PostList.js
+++ redo-02-mini-microservice-app/blog/client/src/PostList.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { createReadStream } from "fs";
+import CommentCreate from "./CommentCreate";
 
 const PostList = () => {
     const [posts, setPosts] = useState({});
@@ -20,6 +20,7 @@ const PostList = () => {
             <div className="card" style={{ width: "30%", marginBottom: "20px" }} key={post.id}>
                 <div className="card-body">
                     <h3>{post.title}</h3>
+                    <CommentCreate postId={post.id} />
                 </div>
             </div>
         );
