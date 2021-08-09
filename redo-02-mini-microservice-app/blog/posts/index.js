03afffaadf393136b07fd7352783ce5e7aa561af
lyx0 <stefan@parfuss.me>
2021-08-09 17:04:08 +0200

    receive /events

--- redo-02-mini-microservice-app/blog/posts/index.js
+++ redo-02-mini-microservice-app/blog/posts/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
+const axios = require("axios");
+
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
@@ -12,7 +14,7 @@ app.get("/posts", (req, res) => {
     res.send(posts);
 });
 
-app.post("/posts", (req, res) => {
+app.post("/posts", async (req, res) => {
     const id = randomBytes(4).toString("hex");
     const { title } = req.body;
 
@@ -21,7 +23,21 @@ app.post("/posts", (req, res) => {
         title,
     };
 
+    await axios.post("http://localhost:4005/events", {
+        type: "PostCreated",
+        data: {
+            id,
+            title,
+        },
+    });
+
     res.status(201).send(posts[id]);
+});
+
+app.post("/events", (req, res) => {
+    console.log("Received event", req.body.type);
+
+    res.send({});
 });
 
 app.listen(4000, () => {
