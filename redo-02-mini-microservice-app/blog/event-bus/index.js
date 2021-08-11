0eaa52d4390bdacab9080aa0550cc5c87f33659c
lyx0 <stefan@parfuss.me>
2021-08-11 18:57:30 +0200

    send back moderation status

--- redo-02-mini-microservice-app/blog/event-bus/index.js
+++ redo-02-mini-microservice-app/blog/event-bus/index.js
@@ -8,13 +8,23 @@ app.use(bodyParser.json());
 app.post("/events", (req, res) => {
     const event = req.body;
 
+    // Posts
     axios.post("http://localhost:4000/events", event).catch((err) => {
         console.log(err.message);
     });
+
+    // Comments
     axios.post("http://localhost:4001/events", event).catch((err) => {
         console.log(err.message);
     });
+
+    // Query
     axios.post("http://localhost:4002/events", event).catch((err) => {
+        console.log(err.message);
+    });
+
+    // Moderation
+    axios.post("http://localhost:4003/events", event).catch((err) => {
         console.log(err.message);
     });
     res.send({ status: "OK" });
