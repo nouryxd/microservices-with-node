6e28aacb8c7b53084a5f9cc267209199f541c9fa
lyx0 <stefan@parfuss.me>
2021-08-11 20:26:59 +0200

    store events

--- redo-02-mini-microservice-app/blog/event-bus/index.js
+++ redo-02-mini-microservice-app/blog/event-bus/index.js
@@ -5,8 +5,12 @@ const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post("/events", (req, res) => {
     const event = req.body;
+
+    events.push(event);
 
     // Posts
     axios.post("http://localhost:4000/events", event).catch((err) => {
@@ -28,6 +32,10 @@ app.post("/events", (req, res) => {
         console.log(err.message);
     });
     res.send({ status: "OK" });
+});
+
+app.get("/events", (req, res) => {
+    res.send(events);
 });
 
 app.listen(4005, () => {
