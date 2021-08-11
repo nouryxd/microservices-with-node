6449512b94bc899715b679d465da789297013652
lyx0 <stefan@parfuss.me>
2021-08-11 20:43:52 +0200

    process events on startup

--- redo-02-mini-microservice-app/blog/query/index.js
+++ redo-02-mini-microservice-app/blog/query/index.js
@@ -49,13 +49,16 @@ app.post("/events", (req, res) => {
 });
 
 app.listen(4002, async () => {
-    console.log("[Query] Listening on 4002");
+    console.log("Listening on 4002");
+    try {
+        const res = await axios.get("http://localhost:4005/events");
 
-    const res = await axios.get("http://localhost:4005/events");
+        for (let event of res.data) {
+            console.log("Processing event:", event.type);
 
-    for (let event of res.data) {
-        console.log("Processing event: ", event.type);
-
-        handleEvent(event.type, event.data);
+            handleEvent(event.type, event.data);
+        }
+    } catch (error) {
+        console.log(error.message);
     }
 });
